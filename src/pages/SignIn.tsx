5fb7c560f6561d8abde01e22d8746a9596cc8e9c
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2024-09-16 12:16:55 +0300

    signin page dark mode

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -18,6 +18,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/auth_context';
 import { useTranslation } from 'react-i18next';
 import { Copyright } from '@mui/icons-material';
+import { useTheme } from '@mui/material/styles';
 
 const defaultTheme = createTheme();
 
@@ -25,6 +26,7 @@ export default function SignIn() {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const { signIn } = useAuth();
+  const theme = useTheme();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -40,77 +42,70 @@ export default function SignIn() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            {t('authPage.signIn')}
-          </Typography>
-          <Box
-            component='form'
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: theme.palette.secondary.main }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          {t('authPage.signIn')}
+        </Typography>
+        <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label={t('authPage.email')}
+            name='email'
+            autoComplete='email'
+            autoFocus
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label={t('authPage.password')}
+            type='password'
+            id='password'
+            autoComplete='current-password'
+          />
+          <FormControlLabel
+            control={<Checkbox value='remember' color='primary' />}
+            label={t('authPage.rememberMe')}
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
           >
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              id='email'
-              label={t('authPage.email')}
-              name='email'
-              autoComplete='email'
-              autoFocus
-            />
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              label={t('authPage.password')}
-              type='password'
-              id='password'
-              autoComplete='current-password'
-            />
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label={t('authPage.rememberMe')}
-            />
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}
-            >
-              {t('authPage.signIn')}
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='#' variant='body2'>
-                  {t('authPage.forgotPassword')}
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='/signup' variant='body2'>
-                  {t('authPage.noAccount')}
-                </Link>
-              </Grid>
+            {t('authPage.signIn')}
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href='#' variant='body2'>
+                {t('authPage.forgotPassword')}
+              </Link>
             </Grid>
-          </Box>
+            <Grid item>
+              <Link href='/signup' variant='body2'>
+                {t('authPage.noAccount')}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 }
